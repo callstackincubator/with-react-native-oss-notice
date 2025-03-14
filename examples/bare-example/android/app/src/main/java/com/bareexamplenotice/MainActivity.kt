50c8a4813d0a3828ff81e59fdae15d94c3b3e87a
Piotr Tomczewski <piotr.tomczewski@callstack.com>
2025-03-14 10:08:21 +0100

    fix: update package name in android files

--- examples/bare-example/android/app/src/main/java/com/bareexamplenotice/MainActivity.kt
+++ examples/bare-example/android/app/src/main/java/com/bareexamplenotice/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.ReactNativeLegalBareExample
+package com.reactnativelegalbareexample
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- examples/bare-example/android/app/src/main/java/com/bareexamplenotice/MainApplication.kt
+++ examples/bare-example/android/app/src/main/java/com/bareexamplenotice/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.ReactNativeLegalBareExample;
+package com.reactnativelegalbareexample;
 
 import android.app.Application
 import com.facebook.react.PackageList
